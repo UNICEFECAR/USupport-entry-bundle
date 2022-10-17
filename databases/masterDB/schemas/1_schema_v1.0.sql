5cf8b0e3ffa1ecc660e28f3104f8ed257610f4b2
Ivan Ivanov <ivan.h.ivanov99@gmail.com>
2022-10-17 01:30:00 +0100

    update local DB schema files with latest init schema SQL

--- databases/masterDB/schemas/1_schema_v1.0.sql
+++ databases/masterDB/schemas/1_schema_v1.0.sql
@@ -8,11 +8,12 @@ CREATE TYPE "service_type" AS ENUM (
   'text_audio_video_consultation'
 );
 
-CREATE TABLE "admins" (
+CREATE TABLE "admin" (
   "id" SERIAL UNIQUE,
   "admin_id" UUID PRIMARY KEY DEFAULT (gen_random_uuid()),
   "name" varchar NOT NULL,
   "surname" varchar NOT NULL,
+  "phone_prefix" varchar,
   "phone" varchar,
   "email" varchar UNIQUE NOT NULL,
   "password" varchar NOT NULL,
@@ -22,7 +23,7 @@ CREATE TABLE "admins" (
   "deleted_at" timestamp DEFAULT NULL
 );
 
-CREATE TABLE "regions" (
+CREATE TABLE "region" (
   "id" SERIAL UNIQUE,
   "region_id" UUID PRIMARY KEY DEFAULT (gen_random_uuid()),
   "name" varchar NOT NULL,
@@ -31,33 +32,32 @@ CREATE TABLE "regions" (
   "deleted_at" timestamp DEFAULT NULL
 );
 
-CREATE TABLE "countries" (
+CREATE TABLE "country" (
   "id" SERIAL UNIQUE,
   "country_id" UUID PRIMARY KEY DEFAULT (gen_random_uuid()),
   "region_id" UUID,
   "name" varchar NOT NULL,
-  "dbs" varchar NOT NULL,
-  "prefix" varchar NOT NULL,
+  "iso" varchar NOT NULL,
   "created_at" timestamp DEFAULT (now()),
   "updated_at" timestamp DEFAULT NULL,
   "deleted_at" timestamp DEFAULT NULL
 );
 
-CREATE TABLE "admins_to_regions" (
+CREATE TABLE "admin_region_links" (
   "id" SERIAL UNIQUE,
   "admin_id" UUID NOT NULL,
   "region_id" UUID NOT NULL,
   "created_at" timestamp DEFAULT (now())
 );
 
-CREATE TABLE "admins_to_countries" (
+CREATE TABLE "admin_country_links" (
   "id" SERIAL UNIQUE,
   "admin_id" UUID NOT NULL,
   "country_id" UUID NOT NULL,
   "created_at" timestamp DEFAULT (now())
 );
 
-CREATE TABLE "services" (
+CREATE TABLE "service" (
   "id" SERIAL UNIQUE,
   "service_id" UUID PRIMARY KEY DEFAULT (gen_random_uuid()),
   "type" service_type NOT NULL,
@@ -66,44 +66,45 @@ CREATE TABLE "services" (
   "deleted_at" timestamp DEFAULT NULL
 );
 
-CREATE TABLE "service_log" (
+CREATE TABLE "service_client_provider_links" (
   "id" SERIAL UNIQUE,
-  "client_id" UUID NOT NULL,
-  "provider_id" UUID NOT NULL,
+  "client_details_id" UUID NOT NULL,
+  "provider_details_id" UUID NOT NULL,
   "service_id" UUID NOT NULL,
   "duration" int,
   "created_at" timestamp DEFAULT (now())
 );
 
-CREATE TABLE "languages" (
+CREATE TABLE "language" (
   "id" SERIAL UNIQUE,
   "language_id" UUID PRIMARY KEY DEFAULT (gen_random_uuid()),
-  "language" varchar NOT NULL,
-  "prefix" varchar NOT NULL,
+  "name" varchar NOT NULL,
+  "iso" varchar NOT NULL,
   "created_at" timestamp DEFAULT (now()),
   "updated_at" timestamp DEFAULT NULL,
   "deleted_at" timestamp DEFAULT NULL
 );
 
-CREATE TABLE "languages_to_country" (
+CREATE TABLE "country_language_links" (
   "id" SERIAL UNIQUE,
   "country_id" UUID NOT NULL,
   "language_id" UUID NOT NULL,
   "created_at" timestamp DEFAULT (now())
 );
 
-ALTER TABLE "countries" ADD FOREIGN KEY ("region_id") REFERENCES "regions" ("region_id");
 
-ALTER TABLE "admins_to_regions" ADD FOREIGN KEY ("admin_id") REFERENCES "admins" ("admin_id");
+ALTER TABLE "country" ADD FOREIGN KEY ("region_id") REFERENCES "region" ("region_id");
 
-ALTER TABLE "admins_to_regions" ADD FOREIGN KEY ("region_id") REFERENCES "regions" ("region_id");
+ALTER TABLE "admin_region_links" ADD FOREIGN KEY ("admin_id") REFERENCES "admin" ("admin_id");
 
-ALTER TABLE "admins_to_countries" ADD FOREIGN KEY ("admin_id") REFERENCES "admins" ("admin_id");
+ALTER TABLE "admin_region_links" ADD FOREIGN KEY ("region_id") REFERENCES "region" ("region_id");
 
-ALTER TABLE "admins_to_countries" ADD FOREIGN KEY ("country_id") REFERENCES "countries" ("country_id");
+ALTER TABLE "admin_country_links" ADD FOREIGN KEY ("admin_id") REFERENCES "admin" ("admin_id");
 
-ALTER TABLE "service_log" ADD FOREIGN KEY ("service_id") REFERENCES "services" ("service_id");
+ALTER TABLE "admin_country_links" ADD FOREIGN KEY ("country_id") REFERENCES "country" ("country_id");
 
-ALTER TABLE "languages_to_country" ADD FOREIGN KEY ("country_id") REFERENCES "countries" ("country_id");
+ALTER TABLE "service_client_provider_links" ADD FOREIGN KEY ("service_id") REFERENCES "service" ("service_id");
 
-ALTER TABLE "languages_to_country" ADD FOREIGN KEY ("language_id") REFERENCES "languages" ("language_id");
+ALTER TABLE "country_language_links" ADD FOREIGN KEY ("country_id") REFERENCES "country" ("country_id");
+
+ALTER TABLE "country_language_links" ADD FOREIGN KEY ("language_id") REFERENCES "language" ("language_id");
