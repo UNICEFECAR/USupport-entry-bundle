40a4619f7705caf330558cde0ea893419138b991
Vasilen Tsvetkov <vasilen@7digit.io>
2022-11-16 01:01:28 +0000

    Refactor: fix sql syntax

--- databases/piiDB/schemas/3_schema_v1.2.sql
+++ databases/piiDB/schemas/3_schema_v1.2.sql
@@ -1,8 +1,9 @@
 ALTER TABLE
   availability
 ALTER COLUMN
-  slots TYPE timestamptz [] USING slots :: timestamp with time zone []
+  slots TYPE timestamptz [] USING slots :: timestamp with time zone [];
+
 ALTER TABLE
   availability
 ALTER COLUMN
-  start_date TYPE timestamptz USING slots :: timestamp with time zone
+  start_date TYPE timestamptz USING start_date :: timestamp with time zone;
