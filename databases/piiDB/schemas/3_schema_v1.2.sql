be17cdca9358a389286c4e2f0fbfc9a79f8cba9e
Vasilen Tsvetkov <vasilen@7digit.io>
2022-11-26 21:32:35 +0000

    Refactor: rename column "living_place" to "urban_rural"

--- databases/piiDB/schemas/3_schema_v1.2.sql
+++ databases/piiDB/schemas/3_schema_v1.2.sql
@@ -7,3 +7,8 @@ ALTER TABLE
   availability
 ALTER COLUMN
   start_date TYPE timestamptz USING start_date :: timestamp with time zone;
+
+ALTER TYPE "living_type" RENAME TO "urban_rural_type";
+
+ALTER TABLE "client_detail" ALTER COLUMN "living_place" TYPE "urban_rural_type";
+ALTER TABLE "client_detail" RENAME COLUMN "living_place" TO "urban_rural";
