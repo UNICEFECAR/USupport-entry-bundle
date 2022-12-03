f64cc5f334a0e8d63b7df92c0708d4cb966632ca
Vasilen Tsvetkov <vasilen@7digit.io>
2022-12-03 22:10:15 +0000

    Refactor: email column constraint within the admin table

--- databases/masterDB/schemas/3_schema_v1.2.sql
+++ databases/masterDB/schemas/3_schema_v1.2.sql
@@ -26,6 +26,7 @@ ALTER TABLE "country" ADD COLUMN "max_client_age" int DEFAULT 24;
 
 ALTER TABLE "admin" ADD COLUMN "is_active" Boolean DEFAULT false;
 ALTER TABLE "admin" ADD COLUMN "deleted_at" timestamp DEFAULT NULL;
+ALTER TABLE "admin" DROP CONSTRAINT "admin_email_key";
 
 ALTER TABLE "service_client_provider_links" RENAME COLUMN "client_details_id" TO "client_detail_id";
 ALTER TABLE "service_client_provider_links" RENAME COLUMN "provider_details_id" TO "provider_detail_id";
