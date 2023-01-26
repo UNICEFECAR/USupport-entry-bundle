32b3665f234c7f9c98a0041252ed8a577c38eaae
Sebastian Galustyan <sebastian@7digit.io>
2023-01-26 23:52:58 +0000

    Add: add 'transaction_log' table to clinical db

---
+++ databases/clinicalDB/schemas/4_schema_v1.3.sql
@@ -0,0 +1,16 @@
+CREATE TYPE "transaction_type" AS ENUM ('card', 'coupon');
+
+CREATE TABLE "transaction_log" (
+  "id" SERIAL UNIQUE,
+  "transaction_log_id" UUID PRIMARY KEY DEFAULT (gen_random_uuid()),
+  "type" transaction_type NOT NULL,
+  "consultation_id" UUID NOT NULL,
+  "payment_intent" varchar,
+  "coupon_id" UUID DEFAULT NULL,
+  "created_at" timestamp DEFAULT (now())
+);
+
+ALTER TABLE "transaction_log" ADD FOREIGN KEY ("consultation_id") REFERENCES "consultation" ("consultation_id");
+
+ALTER TABLE "transaction_log" ADD COLUMN "payment_refund_id" varchar;
+ALTER TYPE "transaction_type" ADD VALUE 'payment_refund';
