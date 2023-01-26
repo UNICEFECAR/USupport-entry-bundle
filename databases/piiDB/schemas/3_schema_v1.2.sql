86989130183ffc6701bf886fdab86512403f3ca7
Georgi 7DIGIT <georgi@7digit.io>
2023-01-26 23:56:29 +0200

    Create: contact form, information portal suggestion and client rating tables

--- databases/piiDB/schemas/3_schema_v1.2.sql
+++ databases/piiDB/schemas/3_schema_v1.2.sql
@@ -20,3 +20,35 @@ ALTER TABLE "provider_detail" ADD "postcode" varchar;
 ALTER TYPE "specializations_type" RENAME VALUE 'coach' TO 'DELETED';
 
 ALTER TABLE "provider_detail" ADD "video_link" varchar;
+
+CREATE TABLE "contact_form" (
+    "id" SERIAL UNIQUE,
+    "contact_form_id" UUID PRIMARY KEY DEFAULT (gen_random_uuid()),
+    "email" varchar NOT NULL,
+    "subject" varchar NOT NULL,
+    "message" varchar NOT NULL,
+    "created_at" timestamp DEFAULT (now())
+);
+
+CREATE TABLE "information_portal_suggestion" (
+    "id" SERIAL UNIQUE,
+    "information_portal_suggestion_id" UUID PRIMARY KEY DEFAULT (gen_random_uuid()),
+    "client_detail_id" UUID NOT NULL,
+    "suggestion" varchar NOT NULL,
+    "created_at" timestamp DEFAULT (now())
+);
+
+ALTER TABLE "information_portal_suggestion" ADD  FOREIGN KEY ("client_detail_id") REFERENCES "client_detail" ("client_detail_id");
+
+
+CREATE TABLE "client_rating" (
+    "id" SERIAL UNIQUE,
+    "client_rating_id" UUID PRIMARY KEY DEFAULT (gen_random_uuid()),
+    "client_detail_id" UUID NOT NULL,
+    "rating" int NOT NULL,
+    "comment" varchar,
+    "created_at" timestamp DEFAULT (now())
+);
+
+ALTER TABLE "client_rating" ADD  FOREIGN KEY ("client_detail_id") REFERENCES "client_detail" ("client_detail_id");
+ALTER TABLE "client_rating" ADD CONSTRAINT check_client_rating_values CHECK (rating BETWEEN 1 AND 5);
