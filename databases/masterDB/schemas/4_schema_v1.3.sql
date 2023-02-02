bb9a1b2a553fffd774d70b974b4c5e1f8f112863
Vasilen Tsvetkov <vasilen@7digit.io>
2023-02-02 21:44:58 +0000

    Add: missing linkage between country and currency for Kazakhstan

--- databases/masterDB/schemas/4_schema_v1.3.sql
+++ databases/masterDB/schemas/4_schema_v1.3.sql
@@ -142,3 +142,4 @@ INSERT INTO currency (name, code, symbol) VALUES ('Rials', 'YER', '﷼');
 INSERT INTO currency (name, code, symbol) VALUES ('Zimbabwe Dollars', 'ZWD', 'Z$');
 INSERT INTO currency (name, code, symbol) VALUES ('Rupees', 'INR', '₹');
 
+INSERT INTO "country_currency_links" ("country_id", "currency_id") VALUES ((SELECT "country_id" FROM "country" WHERE "alpha2" = 'KZ'), (SELECT "currency_id" FROM "currency" WHERE "code" = 'KZT'));
