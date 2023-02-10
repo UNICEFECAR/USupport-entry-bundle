762d69774d6bd20c26e9c3d33fc196a1519b617b
Sebastian Galustyan <sebastian@7digit.io>
2023-02-10 07:57:47 +0000

    Merge branch 'development' of github.com:UNICEFECAR/USupport-entry-bundle into staging

--- databases/clinicalDB/schemas/4_schema_v1.3.sql
+++ databases/clinicalDB/schemas/4_schema_v1.3.sql
@@ -14,3 +14,5 @@ ALTER TABLE "transaction_log" ADD FOREIGN KEY ("consultation_id") REFERENCES "co
 
 ALTER TABLE "transaction_log" ADD COLUMN "payment_refund_id" varchar;
 ALTER TYPE "transaction_type" ADD VALUE 'payment_refund';
+
+ALTER TABLE "transaction_log" RENAME COLUMN "coupon_id" TO "campaign_id";
