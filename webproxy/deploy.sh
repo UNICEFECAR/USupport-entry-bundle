a5f8b7675109c7cc618374f9afe367f6f9e8e2e8
Ivan Ivanov <ivan.h.ivanov99@gmail.com>
2023-03-29 00:51:17 +0100

    refactor webproxy deploy script

--- webproxy/deploy.sh
+++ webproxy/deploy.sh
@@ -1,11 +1,27 @@
-cd ..
+REDEPLOY=$1
 
-docker build -t webproxy:latest -f DockerfileStaging .
+if [ "$REDEPLOY" = 'deploy' ] || [ "$REDEPLOY" = 'redeploy' ]
+then
+    cd ..
+    # Build docker image of the service locally
+    docker build -t webproxy:latest -f DockerfileStaging .
 
-docker tag webproxy:latest 482053628475.dkr.ecr.eu-central-1.amazonaws.com/usupport-webproxy
+    docker tag webproxy:latest 482053628475.dkr.ecr.eu-central-1.amazonaws.com/usupport-webproxy
 
-docker push 482053628475.dkr.ecr.eu-central-1.amazonaws.com/usupport-webproxy
+    # Push image to ECR
+    docker push 482053628475.dkr.ecr.eu-central-1.amazonaws.com/usupport-webproxy
+    cd webproxy
 
-cd webproxy
+    if [ "$REDEPLOY" = 'deploy' ]
+    then
+        # Update Kuberenetes Cluster applications for this API service
+        kubectl apply -f deployment.yaml -f service.yaml
+    elif [ "$REDEPLOY" = 'redeploy' ]
+    then 
+        kubectl apply -f service.yaml
+        kubectl rollout restart deployment webproxy
+    fi
 
-kubectl apply -f deployment.yaml -f service.yaml
+else
+    echo "Please select either to deploy or redeploy k8s pod"
+fi
