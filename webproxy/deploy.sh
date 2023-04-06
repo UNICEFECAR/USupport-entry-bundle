be34bc681f5f6ea351c1dc1a3a7b0826c21ee040
Ivan Ivanov <ivan.h.ivanov99@gmail.com>
2023-04-06 01:23:47 +0100

    add prod config setup

--- webproxy/deploy.sh
+++ webproxy/deploy.sh
@@ -1,26 +1,46 @@
-REDEPLOY=$1
+ENV=$1
+REDEPLOY=$2
+
+if [ "$ENV" != 'staging' ] && [ "$ENV" != 'prod' ]
+then    
+    echo "Please select deployment environment: staging | prod"
+fi
 
 if [ "$REDEPLOY" = 'deploy' ] || [ "$REDEPLOY" = 'redeploy' ]
 then
+    IMAGE_TAG=beta
+    DOCKERFILE=DockerfileStaging
+    if [ "$ENV" = 'staging' ]
+    then    
+        IMAGE_TAG=beta
+        DOCKERFILE=DockerfileStaging
+    elif [ "$ENV" = 'prod' ]
+    then 
+        IMAGE_TAG=latest
+        DOCKERFILE=Dockerfile
+    fi
+
     cd ..
     # Build docker image of the service locally
-    docker build -t webproxy:latest -f DockerfileStaging .
+    docker build -t webproxy:$IMAGE_TAG -f $DOCKERFILE .
+    docker tag webproxy:$IMAGE_TAG 482053628475.dkr.ecr.eu-central-1.amazonaws.com/usupport-webproxy:$IMAGE_TAG
 
-    docker tag webproxy:latest 482053628475.dkr.ecr.eu-central-1.amazonaws.com/usupport-webproxy
+    # Push image to 
+    docker push 482053628475.dkr.ecr.eu-central-1.amazonaws.com/usupport-webproxy:$IMAGE_TAG
 
-    # Push image to ECR
-    docker push 482053628475.dkr.ecr.eu-central-1.amazonaws.com/usupport-webproxy
-    cd webproxy
+    cd webproxy/kube-config/$ENV
 
     if [ "$REDEPLOY" = 'deploy' ]
     then
         # Update Kuberenetes Cluster applications for this API service
-        kubectl apply -f deployment.yaml -f service.yaml
+        kubectl apply -f deployment.yaml
     elif [ "$REDEPLOY" = 'redeploy' ]
     then 
-        kubectl apply -f service.yaml
         kubectl rollout restart deployment webproxy
     fi
+
+    cd ..
+    kubectl apply -f service.yaml
 
 else
     echo "Please select either to deploy or redeploy k8s pod"
