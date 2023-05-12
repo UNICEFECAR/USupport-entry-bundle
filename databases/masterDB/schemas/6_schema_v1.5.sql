fda251c8fa5ac55c6d72f9759e4a0dc06c7e57fd
Georgi Ganchev @7DIGIT <georgi.ganchev@7digit.io>
2023-05-12 23:25:42 +0300

    Create: 6_schema_v1.5.sql for master database

---
+++ databases/masterDB/schemas/6_schema_v1.5.sql
@@ -0,0 +1,23 @@
+ALTER TABLE language ADD COLUMN local_name varchar;
+
+UPDATE language SET local_name = 'Қазақша' WHERE alpha2 = 'kk';
+UPDATE language SET local_name = 'Oʻzbekcha' WHERE alpha2 = 'uz';
+UPDATE language SET local_name = 'Српски' WHERE alpha2 = 'sr';
+UPDATE language SET local_name = 'Hrvatsk' WHERE alpha2 = 'hr';
+UPDATE language SET local_name = 'Shqip' WHERE alpha2 = 'sq';
+UPDATE language SET local_name = 'Türkçe' WHERE alpha2 = 'tk';
+UPDATE language SET local_name = 'հայերեն' WHERE alpha2 = 'hy';
+UPDATE language SET local_name = 'Български' WHERE alpha2 = 'bg';
+UPDATE language SET local_name = 'English' WHERE alpha2 = 'en';
+UPDATE language SET local_name = 'ქართული' WHERE alpha2 = 'ka';
+UPDATE language SET local_name = 'Русский' WHERE alpha2 = 'ru';
+UPDATE language SET local_name = 'Slovenščina' WHERE alpha2 = 'sl';
+UPDATE language SET local_name = 'Українська' WHERE alpha2 = 'uk';
+UPDATE language SET local_name = 'Беларуская' WHERE alpha2 = 'be';
+UPDATE language SET local_name = 'Тоҷикӣ' WHERE alpha2 = 'tg';
+UPDATE language SET local_name = 'Кыргызча' WHERE alpha2 = 'ky';
+UPDATE language SET local_name = 'Română' WHERE alpha2 = 'ro';
+UPDATE language SET local_name = 'Македонски' WHERE alpha2 = 'mk';
+UPDATE language SET local_name = 'Azərbaycan' WHERE alpha2 = 'az';
+UPDATE language SET local_name = 'Bosanski' WHERE alpha2 = 'bs';
+
