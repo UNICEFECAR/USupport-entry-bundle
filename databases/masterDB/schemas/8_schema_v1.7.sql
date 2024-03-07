cdd0f44f481a619235170fb08201bb7b46f59483
Georgi 7DIGIT <georgi@7digit.io>
2024-03-08 00:20:59 +0200

    Fix: add local name when inserting PL country

--- databases/masterDB/schemas/8_schema_v1.7.sql
+++ databases/masterDB/schemas/8_schema_v1.7.sql
@@ -1,2 +1,2 @@
-INSERT INTO "country" ("region_id", "name", "alpha2", "alpha3", "num3", "is_active") VALUES ((SELECT "region_id" FROM "region" WHERE "name" = 'ECARO'), 'Poland', 'PL', 'POL', '616', true);
+INSERT INTO "country" ("region_id", "name", "alpha2", "alpha3", "num3", "is_active", "local_name") VALUES ((SELECT "region_id" FROM "region" WHERE "name" = 'ECARO'), 'Poland', 'PL', 'POL', '616', true), "Polska";
 INSERT INTO "country_currency_links" ("country_id", "currency_id") VALUES ((SELECT "country_id" FROM "country" WHERE "alpha2" = 'PL'), (SELECT "currency_id" FROM "currency" WHERE "code" = 'PLN'));
