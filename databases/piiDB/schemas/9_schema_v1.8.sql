b2a51ac85abb4ee7540dbc1e41cb98a988d3ab1d
Georgi 7DIGIT <georgi@7digit.io>
2024-04-11 15:30:45 +0300

    Add platform_access and provider_rating tables

--- databases/piiDB/schemas/9_schema_v1.8.sql
+++ databases/piiDB/schemas/9_schema_v1.8.sql
@@ -5,3 +5,22 @@ CREATE TABLE "jwt_blacklist" (
     "token" TEXT NOT NULL,
     "created_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
+
+ALTER TABLE "user" ADD COLUMN last_login timestamp DEFAULT NOW();
+
+CREATE TABLE "platform_access" (
+    "id" SERIAL PRIMARY KEY,
+    "user_id" UUID,
+    "ip_address" VARCHAR NOT NULL,
+    "platform" VARCHAR NOT NULL,
+    "created_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE "provider_rating" (
+    "id" SERIAL UNIQUE,
+    "provider_rating_id" UUID PRIMARY KEY DEFAULT (gen_random_uuid()),
+    "provider_detail_id" UUID NOT NULL,
+    "rating" int NOT NULL,
+    "comment" varchar,
+    "created_at" timestamp DEFAULT (now())
+);
