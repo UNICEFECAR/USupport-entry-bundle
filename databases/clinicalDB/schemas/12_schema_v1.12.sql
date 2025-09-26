80f30e093f192251f028b5291a970caa96ce3b93
7Digit-main <it-support@7digit.io>
2025-09-26 12:20:43 +0300

    Add: add new values to the notification type enum clinical db

--- databases/clinicalDB/schemas/12_schema_v1.12.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE "mood_tracker" ADD COLUMN "is_critical" BOOLEAN DEFAULT false;
-ALTER TABLE "mood_tracker" ADD COLUMN "is_deleted" BOOLEAN DEFAULT false;
-ALTER TABLE 'mood_tracker' ADD COLUMN "deleted_at" timestamp DEFAULT NULL;
-
-ALTER TABLE "mood_tracker" ALTER COLUMN "client_detail_id" DROP NOT NULL;
